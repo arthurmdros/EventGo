8e02ef4fa0b957aa2ee76b2d955d14ac080f78aa
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-16 17:21:34 -0300

    Inicializando EventDetail

---
+++ src/screens/EventDetail/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import styles from './styles';
+
+export default function EventDetail(){
+    return(
+        <View style={styles.View}>
+            <Text style={styles.Text}>Event Detail</Text>
+        </View>
+    );
+}
