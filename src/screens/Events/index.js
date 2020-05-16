53c7c85288d0fcfa747a1e8fa490d5c0d57445c1
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-16 16:19:05 -0300

    Inicializando screen Events

---
+++ src/screens/Events/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import './styles';
+
+export default function Events(){
+    return(
+        <View>
+            <Text>Events List</Text>
+        </View>
+    );
+}
