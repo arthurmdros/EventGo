809ea64e55852605a313b849cb4e82882b8f2501
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-16 16:19:38 -0300

    Inicializando estilização da screen Events

---
+++ src/screens/Events/styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({    
+    View: {
+        flex: 1,
+        backgroundColor: '#1393f6',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    text: {
+      fontSize: 16,
+      color: '#FFF',
+    },
+  })
