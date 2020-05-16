46c16b2d8712b8d849e6ef0d23bd543e9a1beb64
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-16 17:42:07 -0300

    Adicioando estilos  nos  componentes de  navegação

--- src/screens/EventDetail/styles.js
+++ src/screens/EventDetail/styles.js
@@ -12,4 +12,16 @@ export default StyleSheet.create({
       fontSize: 16,
       color: '#fff',
     },
+
+    TouchableOpacity: {
+      flexDirection: 'row',
+      backgroundColor: '#FFF',
+      marginTop: 30,
+    },
+
+    TextButton: {
+      fontSize: 16,
+      fontWeight: "bold",
+      color: '#1393f6',
+    }
   })
