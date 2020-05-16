f89e4bd3a3a309e92d832a5682722d5f934429fd
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-16 17:23:52 -0300

    Adicionando rota para screen EventDetail

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 const AppStack = createStackNavigator();
 
 import Events from './screens/Events';
+import EventDetail from './screens/EventDetail';
 
 export default function Routes(){
     return(
         <NavigationContainer>
             <AppStack.Navigator screenOptions={{ headerShown: false}}>
                 <AppStack.Screen name="Events" component={Events}/>
+                <AppStack.Screen name="EventDetail" component={EventDetail}/>
             </AppStack.Navigator>
         </NavigationContainer>
     );
