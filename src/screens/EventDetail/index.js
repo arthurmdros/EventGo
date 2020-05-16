eeb09b6fc86934da9740ad663e2190639062aaf6
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-16 17:41:26 -0300

    Adicionando navegação para screen Ticket

--- src/screens/EventDetail/index.js
+++ src/screens/EventDetail/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import {View, Text, TouchableOpacity} from 'react-native';
 
 import styles from './styles';
 
 export default function EventDetail(){
+    const navigation = useNavigation();
+
     return(
         <View style={styles.View}>
             <Text style={styles.Text}>Event Detail</Text>
+            <TouchableOpacity
+            style={styles.TouchableOpacity}
+            onPress={()=> navigation.navigate('EventDetail')}
+            >
+                <Text style={styles.TextButton}>Event Detail</Text>
+            </TouchableOpacity>
         </View>
     );
 }
