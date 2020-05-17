1c41edc02aad5b48b8e93c32a47478b4c2086753
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-17 16:46:34 -0300

    Adicionando View com as informações de um evento

--- src/screens/EventDetail/index.js
+++ src/screens/EventDetail/index.js
@@ -10,12 +10,33 @@ export default function EventDetail(){
     return(
         <View style={styles.View}>
             <Text style={styles.Text}>Event Detail</Text>
+            <View style={styles.item}>
+                <Text style={[styles.itemProperty, {marginTop: 0}]}>Evento:</Text>
+                <Text style={styles.itemValue}>Evento N°</Text>
+
+                <Text style={styles.itemProperty}>Descrição:</Text>
+                <Text style={styles.itemValue}>Evento criado com tal objetivo</Text>
+
+                <Text style={styles.itemProperty}>Começa dia:</Text>
+                <Text style={styles.itemValue}>00/00/0000</Text>
+
+                <Text style={styles.itemProperty}>Termina dia:</Text>
+                <Text style={styles.itemValue}>00/00/0000</Text>
+
+                <Text style={styles.itemProperty}>Começa às:</Text>
+                <Text style={styles.itemValue}>00:00</Text>
+
+                <Text style={styles.itemProperty}>Termina às:</Text>
+                <Text style={styles.itemValue}>00:00</Text>
+            
             <TouchableOpacity
             style={styles.TouchableOpacity}
-            onPress={()=> navigation.navigate('EventDetail')}
+            onPress={()=> navigation.navigate('Ticket')}
             >
-                <Text style={styles.TextButton}>Event Detail</Text>
+                <Text style={styles.TextButton}>Ticket</Text>
             </TouchableOpacity>
+
+            </View>
         </View>
     );
 }
