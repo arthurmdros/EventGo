70eab49e94ae91ba27797193525fbb25fbd1776b
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-17 16:52:14 -0300

    Adicionando View com as informações do Ingresso

--- src/screens/Ticket/index.js
+++ src/screens/Ticket/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {TouchableOpacity, View, Text} from 'react-native';
 
 import styles from './styles';
 
@@ -7,6 +7,24 @@ export default function Ticket(){
     return(
         <View style={styles.View}>
             <Text style={styles.Text}>Ticket</Text>
+            <View style={styles.item}>
+                <Text style={[styles.itemProperty, {marginTop: 0}]}>Ingresso:</Text>
+                <Text style={styles.itemValue}>Ingresso tal</Text>
+
+                <Text style={styles.itemProperty}>Valor:</Text>
+                <Text style={styles.itemValue}>R$ 00,00</Text>
+
+                <Text style={styles.itemProperty}>Quantidade:</Text>
+                <Text style={styles.itemValue}>1</Text>                
+            
+            <TouchableOpacity
+            style={styles.TouchableOpacity}
+            onPress={()=> alert('Ingresso adquirido, enviando para o seu e-mail')}
+            >
+                <Text style={styles.TextButton}>Buy Ticket</Text>
+            </TouchableOpacity>
+
+            </View>
         </View>
     );
 }
