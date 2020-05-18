1ba5dfd070c727de7b2fc451de1204bdf2425a21
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-18 01:48:03 -0300

    Implementação do loadEvents

--- src/screens/Events/index.js
+++ src/screens/Events/index.js
@@ -1,51 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
-import {FlatList, View, Text, TouchableOpacity} from 'react-native';
+import {FlatList, View, Image, Text, TouchableOpacity} from 'react-native';
 
+import logoImg from '../../assets/logo.png';
+import api from '../../services/api';
 import styles from './styles';
 
 export default function Events(){
 
     const navigation = useNavigation();
 
-    const data = [
-        {
-            id:'1',
-            title: 'Evento 1'
-        },
-        {
-            id:'2',
-            title: 'Evento 2'
-        },
-        {
-            id:'3',
-            title: 'Evento 3'
-        },
-        {
-            id:'4',
-            title: 'Evento 4'
-        },
-    ]
+    const [events, setEvents] = useState([]);
+    const [page, setPage] = useState(1);
+    const [total, setTotal] = useState(0);
+    const [loading, setLoading] = useState(false);
+
+    async function loadEvents(){
+        if(loading){
+            return;
+        }
+
+        if(total > 0 && events.length === total){
+            return;
+        }
+
+        setLoading(true);
+
+        const response = await api.get('event/index', {
+            params: {page}
+        });
+
+        setEvents([...events, ...response.data]);
+        setTotal(response.headers('Total-Events'));
+        setPage(page + 1);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        loadEvents();
+    }, []);
 
     return(
-        <View style={styles.View}>
-            <Text style={styles.Text}>Events List</Text>
+        <View style={styles.eventsContainer}>
+            <View style={styles.header}>
+                <Image source={logoImg}/>
+            </View>
+
+            <Text style={styles.title}>Bem-Vindo ao EventGo</Text>
+            <Text style={styles.description}>
+                Na plataforma você encontra eventos que deseja frequentar e 
+                além disso adquire ingressos que confirmam sua presença.
+            </Text>
+
             <FlatList
-                data={data}
-                style={styles.FlatList}
-                keyExtractor={item => item.id}
+                data={events}
+                style={styles.eventList}
+                keyExtractor={event => String (event.id)}
                 showsVerticalScrollIndicator={false}
-                renderItem={({item}) => (
-                    <View style={styles.item}>
-                        <Text style={styles.itemProperty}>Título:</Text>
-                        <Text style={styles.itemValue}>{item.title}</Text>
+                onEndReached={loadEvents}
+                onEndReachedThreshold={0.2}
+                renderItem={({item: event}) => (
+                    <View style={styles.event}>
+                        <Text style={styles.eventProperty}>Título:</Text>
+                        <Text style={styles.eventValue}>{event.title}</Text>
+
+                        <Text style={styles.eventProperty}>Descrição:</Text>
+                        <Text style={styles.eventValue}>{event.description}</Text>
 
                         <TouchableOpacity
-                            style={styles.TouchableOpacity}
-                            onPress={()=> navigation.navigate('EventDetail')}
+                            style={styles.detailsButton}
+                            onPress={()=> navigation.navigate('EventDetail', {event})}
                         >
-                            <Text style={styles.TextButton}> Ver mais detalhes </Text>
+                            <Text style={styles.detailsButtonText}> Ver mais detalhes </Text>
                             <Feather name='arrow-right' size={16} color={'#1393f6'}/>
                         </TouchableOpacity>                        
                     </View>                    
