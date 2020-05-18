1604416523d55b7027415d35fd6be17edb2d9db0
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-18 01:50:57 -0300

    Atualizando estilização da Screen EventDetail

--- src/screens/EventDetail/styles.js
+++ src/screens/EventDetail/styles.js
@@ -1,49 +1,93 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 
-export default StyleSheet.create({    
-    View: {
+
+export default StyleSheet.create({
+    datailContainer: {
         flex: 1,
-        backgroundColor: '#1393f6',
-        alignItems: 'center',
-        justifyContent: 'center',
+        paddingHorizontal: 24,
         paddingTop: Constants.statusBarHeight + 20,
+        backgroundColor: '#1393f6',
     },
 
-    Text: {
-      fontSize: 16,
-      color: '#fff',
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
 
-    TouchableOpacity: {
+    headerAction: {
       flexDirection: 'row',
-      marginTop: 30,
+      justifyContent: 'space-between',
+      alignItems: 'center',
     },
 
-    TextButton: {
-      fontSize: 16,
-      fontWeight: "bold",
-      color: '#1393f6',
+    headerActionText: {
+      color: '#FFF',
+      fontSize: 16, 
+      fontWeight: 'bold',
+    },
+    
+    event: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#41414d',
+        marginBottom: 16,
+        marginTop: 48,
     },
 
-    item: {
-      padding: 24,
-      borderRadius: 8,
-      backgroundColor: '#41414d',
-      marginBottom: 16,
-      marginTop: 48,
-    },
-
-    itemProperty:{
+    eventProperty:{
         fontSize: 14,
         color: '#FFF',
         fontWeight: 'bold',
         marginTop: 24,
     },
 
-    itemValue: {
+    eventValue: {
         marginTop: 8,
         fontSize: 15,        
         color: '#FFF',
     },
-  })
+
+    dateValue: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+
+    dateProperty: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+
+    scheduleValue: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+
+    scheduleProperty: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+
+    action: {
+        backgroundColor: '#FFF',
+        borderRadius: 8,
+        height: 50,
+        width: '48%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+        marginLeft: 65,
+    },
+
+    actionText: {
+        color: '#1393f6',
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+
+});
