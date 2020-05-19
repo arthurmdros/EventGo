ee2ecdde8874e080784d8a6b5f7978d33422a5e8
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-19 16:15:11 -0300

    Inicializando screen Payment

---
+++ src/screens/Payment/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+export default function Payment(){
+    return(
+        <View style={styles.View}>
+            <Text style={styles.Text}>Payment</Text>
+        </View>
+    );
+}
