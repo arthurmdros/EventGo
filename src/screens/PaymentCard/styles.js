fae64d74e1b38481feeca69d2a7bb2c8603f2911
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-19 17:17:10 -0300

    Inicializando estilos da Screen PaymentCard

---
+++ src/screens/PaymentCard/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+
+export default StyleSheet.create({
+    View: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+        backgroundColor: '#1393f6',
+    },
+
+    Text: {
+        fontSize: 16,
+        color: '#FFF',
+    },
+});
