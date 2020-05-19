a37cdde42c3c15fa89512e96ad7cb693516cd62b
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-18 21:09:29 -0300

    Adicionando rota ConfirmTicket

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ const AppStack = createStackNavigator();
 import Events from './screens/Events';
 import EventDetail from './screens/EventDetail';
 import Ticket from './screens/Ticket';
+import ConfirmTicket from './screens/ConfirmTicket';
 
 export default function Routes(){
     return(
@@ -15,6 +16,7 @@ export default function Routes(){
                 <AppStack.Screen name="Events" component={Events}/>
                 <AppStack.Screen name="EventDetail" component={EventDetail}/>
                 <AppStack.Screen name="Ticket" component={Ticket}/>
+                <AppStack.Screen name="ConfirmTicket" component={ConfirmTicket}/>
             </AppStack.Navigator>
         </NavigationContainer>
     );
