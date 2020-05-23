b2102f04b0e9a64a33148d521024f812fbccd835
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-23 00:23:23 -0300

    Implementação de confirmação de pagamento e envio de no-reply na Screen Payment

--- src/screens/Payment/index.js
+++ src/screens/Payment/index.js
@@ -4,6 +4,7 @@ import { useNavigation, useRoute } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 import { Form } from '@unform/mobile';
 
+import nodemailer from '../../services/nodemailer';
 import Input from '../Component/Input';
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
@@ -12,12 +13,36 @@ export default function Payment(){
     const route = useRoute();
     const formRef = useRef(null);
     const navigation = useNavigation();
-    const data = route.params.info;
-    const ticket = data.selectedTicket;
   
+    const dataTicket = route.params.info;
+    const ticket = dataTicket.selectedTicket;
+    const message = `A EventGo agradece a você que utiliza de nossos serviços para ter uma ótima experiências nos eventos que divulgamos. Abaixo encontra-se as informações do seu ingresso adquirido na plataforma. Mais uma vez obrigado! 
+    
+    Ingresso: ${ticket.type}
+    Quantidade: ${dataTicket.amountValue}
+    Valor Total: R$ ${dataTicket.totalValue}
+    Forma de pagamento: ${dataTicket.paymentSelected}`;
 
-    function confirmPayment(data){
-        console.log(data);
+    async function confirmPayment(data){
+        if(data.mail ===  undefined){
+            alert('Insira um e-mail');
+        }else{
+            const name = 'EventGo';
+            const email = data.mail;
+            const info = {
+                name,
+                email,
+                message
+            };
+            try{
+                alert('Informações do ingresso foram enviadas para seu e-mail.');
+                navigation.navigate('Events');
+                await nodemailer.post('/send', info);                
+            }catch(err){
+                alert('Erro ao confirmar pagamento, tente novamente.');                
+            }
+            
+        }
     }
 
     return(
@@ -36,16 +61,16 @@ export default function Payment(){
                     <Input style={styles.inputValue} name='mail' type='text' />                    
                     
                     <Text style={styles.infoProperty}>Forma de pagamento:</Text>
-                    <Text style={styles.infoValue}>{data.paymentSelected}</Text>
+                    <Text style={styles.infoValue}>{dataTicket.paymentSelected}</Text>
 
                     <Text style={styles.infoProperty}>Ingresso:</Text>
-                    <Text style={styles.infoValue}>{data.selectedTicket.type}</Text>
+                    <Text style={styles.infoValue}>{dataTicket.selectedTicket.type}</Text>
 
                     <Text style={styles.infoProperty}>Quantidade:</Text>
-                    <Text style={styles.infoValue}>{data.amountValue}</Text>
+                    <Text style={styles.infoValue}>{dataTicket.amountValue}</Text>
 
                     <Text style={styles.infoProperty}>Valor da compra:</Text>
-                    <Text style={styles.infoValue}>R$ {data.totalValue}</Text>
+                    <Text style={styles.infoValue}>R$ {dataTicket.totalValue}</Text>
 
                     <View style={styles.actions}>
                         <TouchableOpacity style={styles.action} onPress={() => formRef.current.submitForm()}>
