485fc16e25d3f0ff0b2ae798b2ea6ae3f41081a3
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-23 15:58:13 -0300

    Inicializando Screen FreeTicket

---
+++ src/screens/FreeTicket/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+export default function FreeTicket(){
+    return(
+        <View style={styles.View}>
+            <Text style={styles.Text}>Free Ticket</Text>
+        </View>
+    );
+}
